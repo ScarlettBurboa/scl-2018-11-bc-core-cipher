3831bf5bfc44f4409f935fcc5893abfb532027af
ScarlettBurboa <scarlett.burboa@gmail.com>
2018-12-04 10:35:12 -0300

    Sitio Web Cipher funcionando con css incompleto

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,29 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (strngText, offsetNumber) => {
+    let resulText = "";
+    const complementext = "<h2>Tu texto cifrado es:</h2>";  
+    for (let i = 0; i < strngText.length; i++){
+        if (strngText[i] === " "){
+            resulText = resulText + " ";            
+        } else {
+            resulText = resulText + (String.fromCharCode((strngText.charCodeAt(i) - 65 + offsetNumber) % 26 + 65));
+        }        
+    }
+return complementext + '<br>' + resulText + '<br><br><br>';
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (strngText, offsetNumber) => {
+    const complementext = "<h2>Tu texto cifrado es:</h2>"; 
+    let list = [];
+    for (let i = 0; i < strngText.length; i++){
+        if (strngText[i] === " "){
+            list.push(" ");
+        } else if ((strngText.charCodeAt(i) - 65 - offsetNumber) >= 0 || ((strngText.charCodeAt(i) - 65 - offsetNumber) % 26 + 91) > 90){
+            list.push(String.fromCharCode((strngText.charCodeAt(i) - 65 - offsetNumber) % 26 + 65));
+        } else {
+            list.push(String.fromCharCode((strngText.charCodeAt(i) - 65 - offsetNumber) % 26 + 91));
+            }
+    }
+    let unitedWords = list.join("");
+  return complementext + '<br>' + unitedWords + '<br>';
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-/* Acá va tu código */
+window.onload = () =>{
+    const contcipher = document.getElementById('contentresult');
+        document.getElementById('actioncipher').addEventListener('click', (eventcipher) =>{
+        eventcipher.preventDefault();    
+        let strngText = (document.getElementById("processcipher").value).toUpperCase();
+        let offsetNumber = parseInt(document.getElementById("processoffset").value);
+        contcipher.classList.add('resultext');
+        contcipher.innerHTML = cipher.encode(strngText, offsetNumber);
+
+        let buttonCopy = document.createElement('button');
+        let buttonCopyContent = document.createTextNode('Copiar Texto');
+        buttonCopy.appendChild(buttonCopyContent);
+        buttonCopy.setAttribute("type","button");
+        buttonCopy.classList.add('buttoncopy');
+        document.getElementById("contentresult").appendChild(buttonCopy);
+        buttonCopy.addEventListener('click', (eventcopy) => {
+          eventcopy.preventDefault();
+          let textToCopy = document.querySelector('.texttocopy');
+          var range = document.createRange();
+          range.selectNode(textToCopy);
+          window.getSelection().addRange(range);
+          try {
+            // intentar copiar el contenido seleccionado
+            var resultado = document.execCommand('copy');
+            console.log(resultado ? 'Email copiado' : 'No se pudo copiar el email');
+          } catch(err) {
+            console.log('ERROR al intentar copiar el email');
+          }
+          // eliminar el texto seleccionado
+          window.getSelection().removeAllRanges();
+          // cuando los navegadores lo soporten, habría
+          // que utilizar: removeRange(range)
+          })
+    });
+    const contdechiper = document.getElementById('contentresult');
+    document.getElementById('actiondecipher').addEventListener('click', (eventdechiper) =>{
+        eventdechiper.preventDefault();
+        let strngText = (document.getElementById("processcipher").value).toUpperCase();
+        let offsetNumber = parseInt(document.getElementById("processoffset").value);
+        contdechiper.classList.add('resultext');
+        contdechiper.innerHTML = cipher.decode(strngText, offsetNumber);
+    }); 
+
+}
+
